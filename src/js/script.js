25e4897d4d3150f48ec32ecf2199ae3102d811a0
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2024-11-04 10:25:12 +0700

    add swiper js

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,26 @@ menuIcon.addEventListener('click', () => {
   menuList.classList.toggle('hidden');
 });
 // header end
+
+// swiper js
+const swiperEl = document.querySelector('swiper-container');
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 32,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+});
+swiperEl.initialize();
+// swiper js end
